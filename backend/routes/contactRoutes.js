d33285ae797194b185605e6ffd33f6b37c0e9029
himanshu.kumawat <viddeveloper110@gmail.com>
2026-08-07 14:41:05 +0530

    final

--- backend/routes/contactRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { submitEnquiry } from '../controllers/contactController.js';
-
-const router = express.Router();
-
-router.post('/', submitEnquiry);
-
-export default router;
